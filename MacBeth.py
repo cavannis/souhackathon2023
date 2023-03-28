8abbf42277c864e424e226c2993711a0ff76d0df
cavannis <chappellh@sou.edu>
2023-03-28 12:14:16 -0700

    class creation done partially done

--- MacBeth.py
+++ MacBeth.py
@@ -1,12 +1,12 @@
 from Classgui import GUI
-
+from Thief import thief
 print("Welcome to your exotic Macbeth experience. You will choose how Macbeth starts along his journey and will be able to make unique choices along the journey") 
 
 print("choose what career/job Macbeth has started with in his Journey to become king of scotland. This will affect how easy or hard certain events are")
 
 print("Macbeths possible career options are as follows: Dishwasher, thief, Swordsman, Dog walker, Muffin man, and crazed homeless man")
 
-gui = GUI(["Dishwasher", "Thief" , "Muffin man" , "Swordsman", "Dog walker", "Crazed Homless Man", "Dishwasher"]) # creates the gui where you select your class.
+gui = GUI([ "Thief" , "Muffin man" , "Swordsman", "Dog walker", "Crazed Homless Man", "Dishwasher"]) # creates the gui where you select your class.
 
 gui.mainloop() # run the GUI main loop
 
@@ -15,3 +15,22 @@ Classchoice = gui.get_selected_option() # saves the selection from the class gui
 print("you have selected: " , Classchoice )
 
 print("now that you selected your class, it's time to select your weapon!")
+
+if Classchoice == "Thief":
+    
+    T = thief()
+    
+    weapons = T.getweapons()
+    
+    weapons_list = [weapon.strip() for weapon in weapons.split(",")]
+    
+    options = [f'"{weapon_word.strip()}"' for weapon in weapons_list for weapon_word in weapon.split()]
+    
+    gui = GUI(options)
+    gui.mainloop()
+    Weapon_choice = gui.get_selected_option()
+    
+    print("You selected :", Weapon_choice)
+    
+    
+    
---
+++ Thief.py
@@ -0,0 +1,6 @@
+class thief:
+    def __init__(self):
+        self.weapons = "Knife, Sword, Rubber_chicken"
+
+    def getweapons(self):
+        return self.weapons
